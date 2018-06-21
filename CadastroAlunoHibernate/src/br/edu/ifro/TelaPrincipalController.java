fc8c176f9bd6b8bfed96a31a448fba886f92887b
3019657 <3019657@JIPA-LABIN05-SV.ifro.local>
2018-06-21 14:13:33 -0400

    Preencher campo do combobox

--- CadastroAlunoHibernate/src/br/edu/ifro/TelaPrincipalController.java
+++ CadastroAlunoHibernate/src/br/edu/ifro/TelaPrincipalController.java
@@ -18,6 +18,8 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -46,18 +48,21 @@ public class TelaPrincipalController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("aula");
         EntityManager em = emf.createEntityManager();
-        
-        // HQL 
+
+        // Busca utilizando HQL
         Query query = em.createQuery("SELECT a FROM Aluno as a");
         List<Aluno> alunos = query.getResultList();
-        
+
+        // Converte lista para observable list
         ObservableList ob = FXCollections.observableArrayList(alunos);
+        // Adiciona resultado a Tabela
         tbAlunos.setItems(ob);
-        cb.setItems(ob);
-        
+        // Adiciona resultado ao Combobox
+        cb.setItems(ob);        
+
         em.close();
         emf.close();
-    }    
+    }      
     
     private void gravar(ActionEvent event) {
         Aluno aluno = new Aluno();
@@ -74,6 +79,13 @@ public class TelaPrincipalController implements Initializable {
     private void select(MouseEvent event) {        
         Aluno a = (Aluno) tbAlunos.getSelectionModel().getSelectedItem();
         n1.setText(a.getNome());
+    }
+
+    @FXML
+    private void onPressEnter(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER) {
+            
+        }
     }
     
 }
